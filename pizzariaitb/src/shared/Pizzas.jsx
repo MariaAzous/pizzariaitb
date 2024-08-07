4fa10028d23c807cd2478770b8eafec115aa7ee2
MariaAzous <maria.a.souza199@gmail.com>
2024-08-07 20:59:39 -0300

    2° Atualização

---
+++ pizzariaitb/src/shared/Pizzas.jsx
@@ -0,0 +1,75 @@
+import Margherita from "../assets/imagens/Margherita.png"
+import Pepperoni from "../assets/imagens/Pepperoni.png"
+import QuatroQueijos from "../assets/imagens/QuatroQueijos.png"
+import FrangocomCatupiry from "../assets/imagens/FrangocomCatupiry.png"
+import Portuguesa from "../assets/imagens/Portuguesa.png"
+import Calabresa from "../assets/imagens/Calabresa.png"
+import Vegetariana from "../assets/imagens/Vegetariana.png"
+import Havaiana from "../assets/imagens/Havaiana.png"
+import Bacon from "../assets/imagens/Bacon.png"
+import Mexicana from "../assets/imagens/Mexicana.png"
+
+const Pizzas = [
+
+    {
+    "id": 1,
+    "nome": "Margherita",
+    "descricao": "Molho de tomate, mussarela e  manjericão fresco.",
+    "imagem": Margherita
+    },
+    {
+    "id": 2,
+    "nome": "Pepperoni",
+    "descricao": "Molho de tomate, mussarela e pepperoni.",
+    "imagem": Pepperoni
+    },
+    {
+    "id": 3,
+    "nome": "Quatro Queijos",
+    "descricao": "Molho de tomate, mussarela, gorgonzola, parmesão e provolone.",
+    "imagem": QuatroQueijos
+    },
+    {
+        "id": 4,
+        "nome": "Frango com Catupiry",
+        "descricao": "Molho de tomate, mussarela, frango desfiado e catupiry.",
+        "imagem": FrangocomCatupiry
+        },
+        {
+        "id": 5,
+        "nome": "Portuguesa",
+        "descricao": "Molho de tomate, mussarela, presunto, ovo, cebola, azeitona e orégano.",
+        "imagem": Portuguesa
+        },
+        {
+        "id": 6,
+        "nome": "Calabresa",
+        "descricao": "Molho de tomate, mussarela, calabresa e cebola.",
+        "imagem": Calabresa
+        },
+        {
+        "id": 7,
+        "nome": "Vegetariana",
+        "descricao": "Molho de tomate, mussarela, pimentão, cebola, azeitona, champignon e tomate.",
+        "imagem": Vegetariana
+        },
+        {
+        "id": 8,
+        "nome": "Havaiana",
+        "descricao": "Molho de tomate, mussarela, presunto e abacaxi.",
+        "imagem": Havaiana
+        },
+        {
+        "id": 9,
+        "nome": "Bacon",
+        "descricao": "Molho de tomate, mussarela, bacon e cebola.",
+        "imagem": Bacon
+        },
+        {
+        "id": 10,
+        "nome": "Mexicana",
+        "descricao": "Molho de tomate, mussarela, carne moída, pimenta jalapeño, cebola e coentro.",
+        "imagem": Mexicana
+        },
+]
+export default Pizzas
